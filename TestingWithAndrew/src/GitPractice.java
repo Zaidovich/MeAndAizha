628a004474d61bfa1d06f161582d4ba533393e0b
aizhamalkenenbaeva <aikosha1954@gmail.com>
2021-07-20 22:44:57 -0400

    my code

--- TestingWithAndrew/src/GitPractice.java
+++ TestingWithAndrew/src/GitPractice.java
@@ -2,5 +2,6 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("My name is Faridun. What is your name?");
         System.out.printf("My name is Andrew");
+        System.out.println("My name is Aizhamal . Nice to meet you guys!");
     }
 }
